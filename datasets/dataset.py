cc7da02f6077479bb633fc9b1ce676f0ffe505fd
arsity <43494998+arsity@users.noreply.github.com>
2025-07-18 00:32:27 +0000

    Add .gitignore and implement ExlPoseDataset class for dataset handling
    - Added .gitignore to exclude checkpoint, cache, and results files.
    - Introduced ExlPoseDataset class in `exlpose_dataset.py` for loading and processing images from the ExLPose dataset.
    - Updated `dataset.py` to utilize ExlPoseDataset for validation data loading.
    - Enhanced image handling and data augmentation methods in the dataset classes.

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,8 +1,59 @@
 import os
+
+import cv2
+import einops
 import torch
 import torch.utils.data
+from crowdposetools.coco import COCO
 from PIL import Image
-from datasets.data_augment import PairCompose, PairToTensor, PairRandomHorizontalFilp
+from torch.utils.data import Dataset
+
+from datasets.data_augment import PairCompose, PairRandomHorizontalFilp, PairToTensor
+
+EXLPOSE_ROOT = "/workspace/ExLPose"
+
+
+class ExlPoseDataset(Dataset):
+    def __init__(
+        self, dir, patch_size=512, filelist=None, train=False, split="exlpose"
+    ):
+        assert not train, "ExlposeDataset is only for evaluation"
+        assert split in ["exlpose", "a7m3", "ricoh3"], f"{split=}"
+
+        self.patch_size = patch_size
+
+        if split == "exlpose":
+            self.coco = COCO(
+                os.path.join(EXLPOSE_ROOT, "Annotations", "ExLPose_test_LL-A.json")
+            )
+        elif split == "a7m3":
+            self.coco = COCO(
+                os.path.join(EXLPOSE_ROOT, "Annotations", "ExLPose-OC_test_A7M3.json")
+            )
+        elif split == "ricoh3":
+            self.coco = COCO(
+                os.path.join(EXLPOSE_ROOT, "Annotations", "ExLPose-OC_test_RICOH3.json")
+            )
+
+        self.img_ids = sorted(self.coco.getImgIds())
+
+    def get_image(self, index):
+        img_id = self.img_ids[index]
+        img_info = self.coco.loadImgs(img_id)[0]
+        img_path = os.path.join(EXLPOSE_ROOT, img_info["file_name"])
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR_RGB)
+        return img, f"{img_id}.png"
+
+    def __getitem__(self, index):
+        res, img_id = self.get_image(index)
+        res = einops.rearrange(res, "h w c -> c h w")
+        res = torch.from_numpy(res).float() / 255.0
+        res = torch.cat([res, res.clone()], dim=0)
+
+        return res, img_id
+
+    def __len__(self):
+        return len(self.img_ids)
 
 
 class LLdataset:
@@ -10,21 +61,40 @@ class LLdataset:
         self.config = config
 
     def get_loaders(self):
-        train_dataset = AllWeatherDataset(self.config.data.data_dir,
-                                          patch_size=self.config.data.patch_size,
-                                          filelist='{}_train.txt'.format(self.config.data.train_dataset))
-        val_dataset = AllWeatherDataset(self.config.data.data_dir,
-                                        patch_size=self.config.data.patch_size,
-                                        filelist='{}_val.txt'.format(self.config.data.val_dataset), train=False)
+        # train_dataset = AllWeatherDataset(
+        #     self.config.data.data_dir,
+        #     patch_size=self.config.data.patch_size,
+        #     filelist="{}_train.txt".format(self.config.data.train_dataset),
+        # )
+        # val_dataset = AllWeatherDataset(
+        #     self.config.data.data_dir,
+        #     patch_size=self.config.data.patch_size,
+        #     filelist="{}_val.txt".format(self.config.data.val_dataset),
+        #     train=False,
+        # )
+        val_dataset = ExlPoseDataset(
+            self.config.data.data_dir,
+            patch_size=self.config.data.patch_size,
+            filelist="{}_val.txt".format(self.config.data.val_dataset),
+            train=False,
+        )
 
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=self.config.training.batch_size,
-                                                   shuffle=True, num_workers=self.config.data.num_workers,
-                                                   pin_memory=True)
-        val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=self.config.sampling.batch_size,
-                                                 shuffle=False, num_workers=self.config.data.num_workers,
-                                                 pin_memory=True)
+        # train_loader = torch.utils.data.DataLoader(
+        #     train_dataset,
+        #     batch_size=self.config.training.batch_size,
+        #     shuffle=True,
+        #     num_workers=self.config.data.num_workers,
+        #     pin_memory=True,
+        # )
+        val_loader = torch.utils.data.DataLoader(
+            val_dataset,
+            batch_size=self.config.sampling.batch_size,
+            shuffle=False,
+            num_workers=self.config.data.num_workers,
+            pin_memory=True,
+        )
 
-        return train_loader, val_loader
+        return None, val_loader
 
 
 class AllWeatherDataset(torch.utils.data.Dataset):
@@ -42,21 +112,16 @@ class AllWeatherDataset(torch.utils.data.Dataset):
         self.patch_size = patch_size
 
         if train:
-            self.transforms = PairCompose([
-                PairRandomHorizontalFilp(),
-                PairToTensor()
-            ])
+            self.transforms = PairCompose([PairRandomHorizontalFilp(), PairToTensor()])
         else:
-            self.transforms = PairCompose([
-                PairToTensor()
-            ])
+            self.transforms = PairCompose([PairToTensor()])
 
     def get_images(self, index):
-        input_name = self.input_names[index].replace('\n', '')
+        input_name = self.input_names[index].replace("\n", "")
 
-        low_img_name, high_img_name = input_name.split(' ')[0], input_name.split(' ')[1]
+        low_img_name, high_img_name = input_name.split(" ")[0], input_name.split(" ")[1]
 
-        img_id = low_img_name.split('/')[-1]
+        img_id = low_img_name.split("/")[-1]
         low_img, high_img = Image.open(low_img_name), Image.open(high_img_name)
 
         low_img, high_img = self.transforms(low_img, high_img)
--- models/restoration.py
+++ models/restoration.py
@@ -1,9 +1,12 @@
-import torch
-import numpy as np
-import utils
 import os
 import time
+
+import numpy as np
+import torch
 import torch.nn.functional as F
+from tqdm import tqdm
+
+import utils
 
 
 class DiffusiveRestoration:
@@ -17,26 +20,25 @@ class DiffusiveRestoration:
             self.diffusion.load_ddm_ckpt(args.resume, ema=False)
             self.diffusion.model.eval()
         else:
-            print('Pre-trained model path is missing!')
+            print("Pre-trained model path is missing!")
 
     def restore(self, val_loader):
-        image_folder = os.path.join(self.args.image_folder, self.config.data.val_dataset)
+        image_folder = os.path.join(
+            self.args.image_folder, self.config.data.val_dataset
+        )
         with torch.no_grad():
-            for i, (x, y) in enumerate(val_loader):
-
+            for i, (x, y) in enumerate(tqdm(val_loader)):
                 x_cond = x[:, :3, :, :].to(self.diffusion.device)
                 b, c, h, w = x_cond.shape
                 img_h_64 = int(64 * np.ceil(h / 64.0))
                 img_w_64 = int(64 * np.ceil(w / 64.0))
-                x_cond = F.pad(x_cond, (0, img_w_64 - w, 0, img_h_64 - h), 'reflect')
+                x_cond = F.pad(x_cond, (0, img_w_64 - w, 0, img_h_64 - h), "reflect")
 
                 t1 = time.time()
-                pred_x = self.diffusion.model(torch.cat((x_cond, x_cond),
-                                                        dim=1))["pred_x"][:, :, :h, :w]
+                pred_x = self.diffusion.model(torch.cat((x_cond, x_cond), dim=1))[
+                    "pred_x"
+                ][:, :, :h, :w]
                 t2 = time.time()
 
                 utils.logging.save_image(pred_x, os.path.join(image_folder, f"{y[0]}"))
-                print(f"processing image {y[0]}, time={t2 - t1}")
-
-
-
+                tqdm.write(f"processing image {y[0]}, time={t2 - t1}")
